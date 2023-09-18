89a4ec80a8659a822d9719cc2f49ea661686a256
douglasgvr <douglasfarmaangra@gmail.com>
2023-09-18 17:49:12 -0300

    ajustes finais da tela home

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ece82082722da936"; }
+/******/ 		__webpack_require__.h = function() { return "89a6ac19b9725ef7"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
